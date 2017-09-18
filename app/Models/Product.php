b9d634f556dfaa89d2262cb462a731c97ea8648b
cargie <cargiejayectin@gmail.com>
2017-09-18 14:19:41 +0800

    withTrashed on category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -105,7 +105,8 @@ class Product extends Model
 
     public function category()
     {
-        return $this->belongsTo(Category::class, 'category_id');
+        return $this->belongsTo(Category::class, 'category_id')
+            ->withTrashed();
     }
 
     public function tags()
