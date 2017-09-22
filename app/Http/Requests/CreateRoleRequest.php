b1999810e650fbd0b5816bda980a29e0142b0ce3
cargie <cargiejayectin@gmail.com>
2017-09-22 17:02:06 +0800

    validate permission if exists

--- app/Http/Requests/CreateRoleRequest.php
+++ app/Http/Requests/CreateRoleRequest.php
@@ -27,6 +27,14 @@ class CreateRoleRequest extends FormRequest
         return [
             'name' => 'required|unique:roles,name',
             'permissions' => 'required|array',
+            'permissions.*' => 'exists:permissions,id',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'permissions.*.exists' => 'The selected permission is invalid.'
         ];
     }
 }
