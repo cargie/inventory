3dfa719e30450dee53bc15496ef4f54799a4cfac
cargie <cargiejayectin@gmail.com>
2017-09-28 16:36:48 +0800

    save collapse state

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -162,6 +162,19 @@
                 radioClass: 'iradio_square-green',
                 increaseArea: '20%' // optional
             });
+            var $body = $('body');
+            // On click, capture state and save it in localStorage
+            $($.AdminLTE.options.sidebarToggleSelector).click(function () {
+                localStorage.setItem('sidebar', $body.hasClass('sidebar-collapse') ? 1 : 0);
+            });
+
+            // On ready, read the set state and collapse if needed
+            if (localStorage.getItem('sidebar') === '0') {
+                $body.addClass('disable-animations sidebar-collapse');
+                requestAnimationFrame(function () {
+                    $body.removeClass('disable-animations');
+                });
+            }
         })
     </script>
     @yield('scripts')
